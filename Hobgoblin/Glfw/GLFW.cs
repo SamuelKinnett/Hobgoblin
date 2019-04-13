97a37fc09da3ebe5757ae8a5a8cc09ffa536c32d
Samuel Kinnett <samuelkinnett96@googlemail.com>
2019-04-13 17:22:49 +0100

    Small refactor of dll imports

--- Hobgoblin/Glfw/GLFW.cs
+++ Hobgoblin/Glfw/GLFW.cs
@@ -15,18 +15,38 @@ namespace Hobgoblin.Glfw
         private const string GLFW_DLL = "libs/glfw3";
 
         // Public
-        [DllImport(GLFW_DLL, EntryPoint = "glfwInit")] public static extern bool Init();
-        [DllImport(GLFW_DLL, EntryPoint = "glfwCreateWindow")] public static extern IntPtr CreateWindow(int width, int height, string title, IntPtr monitor, IntPtr share);
-        [DllImport(GLFW_DLL, EntryPoint = "glfwMakeContextCurrent")] public static extern void MakeContextCurrent(IntPtr window);
-        [DllImport(GLFW_DLL, EntryPoint = "glfwSwapBuffers")] public static extern void SwapBuffers(IntPtr window);
-        [DllImport(GLFW_DLL, EntryPoint = "glfwGetProcAddress")] public static extern IntPtr GetProcAddress(string procname);
-        [DllImport(GLFW_DLL, EntryPoint = "glfwPollEvents")] public static extern void PollEvents();
-        [DllImport(GLFW_DLL, EntryPoint = "glfwWindowShouldClose")] public static extern int WindowShouldClose(IntPtr window);
-        [DllImport(GLFW_DLL, EntryPoint = "glfwTerminate")] public static extern void Terminate();
-        [DllImport(GLFW_DLL, EntryPoint = "glfwSetWindowSizeCallback")] public static extern WindowSizeFunction SetWindowSizeCallback(IntPtr window, WindowSizeFunction cbfun);
+        [DllImport(GLFW_DLL, EntryPoint = "glfwInit")]
+        public static extern bool Init();
+
+        [DllImport(GLFW_DLL, EntryPoint = "glfwCreateWindow")]
+        public static extern IntPtr CreateWindow(
+            int width, int height, string title, IntPtr monitor, IntPtr share);
+
+        [DllImport(GLFW_DLL, EntryPoint = "glfwMakeContextCurrent")]
+        public static extern void MakeContextCurrent(IntPtr window);
+
+        [DllImport(GLFW_DLL, EntryPoint = "glfwSwapBuffers")]
+        public static extern void SwapBuffers(IntPtr window);
+
+        [DllImport(GLFW_DLL, EntryPoint = "glfwGetProcAddress")]
+        public static extern IntPtr GetProcAddress(string procname);
+
+        [DllImport(GLFW_DLL, EntryPoint = "glfwPollEvents")]
+        public static extern void PollEvents();
+
+        [DllImport(GLFW_DLL, EntryPoint = "glfwWindowShouldClose")]
+        public static extern int WindowShouldClose(IntPtr window);
+
+        [DllImport(GLFW_DLL, EntryPoint = "glfwTerminate")]
+        public static extern void Terminate();
+
+        [DllImport(GLFW_DLL, EntryPoint = "glfwSetWindowSizeCallback")]
+        public static extern WindowSizeFunction SetWindowSizeCallback(
+            IntPtr window, WindowSizeFunction cbfun);
 
         // Private
-        [DllImport(GLFW_DLL, EntryPoint = "glfwWindowHint")] private static extern void WindowHint(Int32 hint, Int32 value);
+        [DllImport(GLFW_DLL, EntryPoint = "glfwWindowHint")]
+        private static extern void WindowHint(Int32 hint, Int32 value);
 
         public static void WindowHint(WindowHint hint, int value)
         {
