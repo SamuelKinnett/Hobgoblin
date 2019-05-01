2f5b3deccdc686532662690493195940151bd9ff
Samuel Kinnett <samuelkinnett96@googlemail.com>
2019-05-01 07:19:22 +0100

    Added test app

---
+++ TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Hobgoblin;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Hob hob = new Hob(800, 600);
+            hob.Scenes.AddScene("MAIN", new TestScene());
+            hob.Initialise();
+            hob.Run();
+        }
+    }
+}
---
+++ TestApp/TestScene.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Hobgoblin.Scenes.Abstract;
+using Hobgoblin.Input.Abstract;
+using Hobgoblin.Glfw.Enums.Input;
+using Hobgoblin.Graphics.Primitives;
+
+namespace TestApp
+{
+    class TestScene : Scene
+    {
+        private Triangle triangle;
+
+        public override void Load(IntPtr window)
+        {
+            triangle = new Triangle(
+                new Vector3(-0.5f, -0.5f, 0.0f),
+                new Vector3(0.5f, -0.5f, 0.0f),
+                new Vector3(0.0f, 0.5f, 0.0f));
+        }
+
+        public override void Render()
+        {
+            triangle.Render();
+        }
+
+        public override void UnLoad()
+        {
+        }
+
+        public override void Update(IInputManager inputManager, IntPtr window)
+        {
+            if (inputManager.IsPressed(Key.Escape, window)) {
+                NextScene = null;
+                ChangeScene = true;
+            }
+        }
+    }
+}
